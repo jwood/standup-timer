8c606c309ead9db6215889603d1fd5cf08d1310e
John Wood <john@interactivemediums.com>
2009-11-23 07:01:13 -0600

    Added an about screen, accessible via the menu

---
+++ src/net/johnpwood/android/standuptimer/About.java
@@ -0,0 +1,12 @@
+package net.johnpwood.android.standuptimer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class About extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
--- src/net/johnpwood/android/standuptimer/ConfigureStandupTimer.java
+++ src/net/johnpwood/android/standuptimer/ConfigureStandupTimer.java
@@ -6,6 +6,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
@@ -23,6 +26,24 @@ public class ConfigureStandupTimer extends Activity implements OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         initializeGUIElements();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.about:
+            startActivity(new Intent(this, About.class));
+            return true;
+        }
+        return false;
     }
 
     public void onClick(View v) {
