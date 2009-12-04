ace7c686fbf5315efe9f5e62930b0d873c3df072
John Wood <john@interactivemediums.com>
2009-12-04 07:19:01 -0600

    Cleaned up the About view.

--- standup-timer/src/net/johnpwood/android/standuptimer/About.java
+++ standup-timer/src/net/johnpwood/android/standuptimer/About.java
@@ -2,11 +2,23 @@ package net.johnpwood.android.standuptimer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class About extends Activity {
+public class About extends Activity implements OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
+        View okButton = findViewById(R.id.about_ok);
+        okButton.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+        case R.id.about_ok:
+            finish();
+            break;
+        }
     }
 }
