7ccee1de0230e6351cb53f3970f2559f8990ff5d
John Wood <john@interactivemediums.com>
2009-12-17 08:17:43 -0600

    Add methods to Team to interact with the TeamDAO.  All access to the TeamDAO should now go through this class.

--- standup-timer/src/net/johnpwood/android/standuptimer/model/Team.java
+++ standup-timer/src/net/johnpwood/android/standuptimer/model/Team.java
@@ -1,16 +1,27 @@
 package net.johnpwood.android.standuptimer.model;
 
+import java.util.List;
+
+import net.johnpwood.android.standuptimer.dao.DAOFactory;
+import net.johnpwood.android.standuptimer.dao.TeamDAO;
+import net.johnpwood.android.standuptimer.utils.Logger;
+import android.content.Context;
+
 public class Team {
     private Long id = null;
     private String name = null;
+    private static DAOFactory daoFactory = DAOFactory.getInstance();
 
     public Team(String name) {
-        this.name = name;
+        if (name == null) {
+            throw new IllegalArgumentException("Team name must not be null");
+        }
+        this.name = name.trim();
     }
 
     public Team(Long id, String name) {
+        this(name);
         this.id = id;
-        this.name = name;
     }
 
     public Long getId() {
@@ -19,5 +30,53 @@ public class Team {
 
     public String getName() {
         return name;
+    }
+
+    public void delete(Context context) {
+        TeamDAO dao = null;
+        try {
+            dao = daoFactory.getTeamDAO(context);
+            dao.delete(this);
+        } finally {
+            if (dao != null) {
+                dao.close();
+            }
+        }
+    }
+
+    public static void create(String name, Context context) {
+        TeamDAO dao = null;
+        try {
+            dao = daoFactory.getTeamDAO(context);
+            dao.save(new Team(name));
+        } catch (Exception e) {
+            Logger.e(e.getMessage());
+        } finally {
+            dao.close();
+        }
+    }
+
+    public static Team findByName(String teamName, Context context) {
+        TeamDAO dao = null;
+        try {
+            dao = daoFactory.getTeamDAO(context);
+            return dao.findByName(teamName);
+        } finally {
+            if (dao != null) {
+                dao.close();
+            }
+        }
+    }
+
+    public static List<String> findAllTeamNames(Context context) {
+        TeamDAO dao = null;
+        try {
+            dao = daoFactory.getTeamDAO(context);
+            return dao.findAllTeamNames();
+        } finally {
+            if (dao != null) {
+                dao.close();
+            }
+        }
     }
 }
