771a045b66bfd393fbfa4bf9256b2209ef3556e2
John Wood <john@interactivemediums.com>
2010-03-18 07:09:06 -0500

    Link-ify the project URL in the about view.

--- standup-timer/src/net/johnpwood/android/standuptimer/About.java
+++ standup-timer/src/net/johnpwood/android/standuptimer/About.java
@@ -1,6 +1,8 @@
 package net.johnpwood.android.standuptimer;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -10,8 +12,11 @@ public class About extends Activity implements OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
+
         View okButton = findViewById(R.id.about_ok);
         okButton.setOnClickListener(this);
+        View projectUrl = findViewById(R.id.project_url);
+        projectUrl.setOnClickListener(this);
     }
 
     public void onClick(View v) {
@@ -19,6 +24,15 @@ public class About extends Activity implements OnClickListener {
         case R.id.about_ok:
             finish();
             break;
+        case R.id.project_url:
+            openProjectUrlInBrowser();
+            break;
         }
+    }
+
+    private void openProjectUrlInBrowser() {
+        Uri uri = Uri.parse(getString(R.string.about_project_url));
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
     }
 }
